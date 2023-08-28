80b86407e2c792d3975e38ae1f3a404087827f57
nachofen <124267664+nachofen@users.noreply.github.com>
2023-08-28 09:12:42 -0700

    3tostr

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""script"""
+
+import math
+
+
+def to_str(n: float) -> str:
+    """takes a float n as argument and returns the string rep of the float"""
+    return str(math.floor(n))
