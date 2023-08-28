df2e0a2cd66adeed5ab831ded4d41daf7875c121
nachofen <124267664+nachofen@users.noreply.github.com>
2023-08-28 09:09:43 -0700

    2floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""script"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """takes a string str1 and a string str2 as args and returns concat str"""
+    return math.floor(n)
