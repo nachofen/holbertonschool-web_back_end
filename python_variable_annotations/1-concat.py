453f94537b24317fa50edc7ac646b76eb8e39a7a
nachofen <124267664+nachofen@users.noreply.github.com>
2023-08-28 09:01:51 -0700

    1concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""script"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """takes a string str1 and a string str2 as args and returns concat str"""
+    return str(str1 + str2)
