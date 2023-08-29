0bea82318d773277c3979c3c142830d9194732c6
nachofen <124267664+nachofen@users.noreply.github.com>
2023-08-29 10:44:21 -0700

    6-mixedlist

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""script"""
+from typing import List, Union
+
+
+def sum_mixed_list(input_list: List[Union[float, int]]) -> float:
+    """calculate the sum of a list of floats or ints
+    Args:
+        input_list: list of floats or ints to be summed
+    Returns:
+        float: the sum of the input list
+    """
+    return sum(input_list)
