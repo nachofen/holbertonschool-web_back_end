838ea64a66373db37df8c7442e801ff8a645c634
nachofen <124267664+nachofen@users.noreply.github.com>
2023-08-29 10:40:02 -0700

    5-sum

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""script"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """calculate the sum of a list of floats
+    Args:
+        input_list: list of floats to be summed
+    Returns:
+        float: the sum of the input list
+    """
+    return sum(input_list)
