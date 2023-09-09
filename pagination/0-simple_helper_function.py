3089fea0664ab89184103d776c53c638d5719c62
nachofen <124267664+nachofen@users.noreply.github.com>
2023-09-09 14:50:26 -0700

    pycodestyle + documentation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """Task0"""
 
-def index_range(page:int, page_size:int):
-    starting_index = (page -1) * page_size
+
+def index_range(page: int, page_size: int):
+    """ return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters."""
+    starting_index = (page - 1) * page_size
     ending_index = starting_index + page_size
     result = (starting_index, ending_index)
     return result
-
