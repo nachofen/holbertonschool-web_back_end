5c02ed35ce244ba3ae0ac7028ed90918f8daa3ff
nachofen <124267664+nachofen@users.noreply.github.com>
2023-09-09 15:23:13 -0700

    task1

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Task 1"""
+
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Use assert to verify that both arguments are integers greater than 0
+        Use index_range to find the correct indexes to paginate the dataset
+        correctly and return the appropriate page of the dataset
+        (i.e. the correct list of rows).
+        If the input arguments are out of range for the dataset, an
+        empty list should be returned."""
+        assert page > 0
+        assert page_size > 0
+        dataset = self.dataset
+        start, end = index_range(page, page_size)
+        if start > len(dataset):
+            return []
+        return dataset[start:end]
+
+
+
+def index_range(page: int, page_size: int):
+    """ return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters."""
+    starting_index = (page - 1) * page_size
+    ending_index = starting_index + page_size
+    result = (starting_index, ending_index)
+    return result
