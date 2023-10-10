90dc593046a31fd58cc8678203f7f2282a5e0d75
nachofen <124267664+nachofen@users.noreply.github.com>
2023-10-10 13:32:44 -0700

    task6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, filename) {
+  try {
+    const [userResult, photoResult] = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]);
+    
+    return [
+      {
+        status: userResult.status,
+        value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason.toString(),
+      },
+      {
+        status: photoResult.status,
+        value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason.toString(),
+      },
+    ];
+  } catch (error) {
+    throw error;
+  }
+}
