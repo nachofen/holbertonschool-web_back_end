d32cd600afdbc0cfa555fa61096b4952064fe5ba
nachofen <124267664+nachofen@users.noreply.github.com>
2023-10-10 13:37:31 -0700

    task6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,14 +5,14 @@ export default async function handleProfileSignup(firstName, lastName, filename)
   const [userResult, photoResult] = await Promise.allSettled([signUpUser(firstName, lastName),
     uploadPhoto(filename)]);
 
-    return [
-      {
-        status: userResult.status,
-        value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason.toString(),
-      },
-      {
-        status: photoResult.status,
-        value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason.toString(),
-      },
-    ];
+  return [
+    {
+      status: userResult.status,
+      value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason.toString(),
+    },
+    {
+      status: photoResult.status,
+      value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason.toString(),
+    },
+  ];
 }
