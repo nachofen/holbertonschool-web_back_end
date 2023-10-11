ae9c74100916bd6b02f058df35d3cbecce4320a6
nachofen <zorba037@gmail.com>
2023-10-11 10:23:18 -0700

    task3

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,5 @@
+export default function getStudentIdsSum(studentList){
+  const onlyIds = studentList.map((student) => student.id);
+  const sum = onlyIds.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  return sum;
+}
