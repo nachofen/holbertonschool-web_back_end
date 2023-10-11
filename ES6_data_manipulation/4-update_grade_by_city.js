e46a92239077b6312dc3e1618d19ccdfe562a925
nachofen <zorba037@gmail.com>
2023-10-11 10:30:53 -0700

    task4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,19 @@
+export default function updateStudentGradeByCity(listStudents, city, newGrades) {
+  const updatedStudents = listStudents
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
+      let grade = 0;
+      if (matchingGrade) {
+        grade = matchingGrade.grade;
+      } else {
+        grade = 'N/A';
+      }
+      return {
+        ...student,
+        grade,
+      };
+    });
+  
+  return updatedStudents;
+}
