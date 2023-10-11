66be6df0bb4d9a7ade6a07b7d89079d33525025b
nachofen <zorba037@gmail.com>
2023-10-11 10:25:12 -0700

    eslin

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
-export default function getStudentIdsSum(studentList){
+export default function getStudentIdsSum(studentList) {
   const onlyIds = studentList.map((student) => student.id);
   const sum = onlyIds.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
   return sum;
